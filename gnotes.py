9f48bf7c594b2b21c2b58136cc95292aad1c01b6
u1375369 <steven.blanc521@gmail.com>
2025-01-17 20:26:23 -0700

    added outline to gnotes.py

---
+++ gnotes.py
@@ -0,0 +1,35 @@
+import os
+
+
+def get_folder_name():
+    pass
+
+def get_folder_path():
+    pass
+
+def get_github_account():
+    pass
+
+def update_bashrc():
+    pass
+
+def update_vimrc():
+    pass
+
+def create_notes_folder():
+    pass
+
+
+def main():
+    name = get_folder_name()
+    path = get_folder_path()
+    account = get_github_account()
+
+    update_bashrc()
+    update_vimrc()
+
+    create_notes_folder()
+    
+
+if __name__ == "__main__":
+    main()
